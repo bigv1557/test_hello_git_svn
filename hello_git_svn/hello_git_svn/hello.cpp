f7c96852c6516202ce7acbd9286474a580fa45f2
bigv1557 <36464499+bigv1557@users.noreply.github.com>
2018-02-15 04:08:21 +0000

    2nd update

--- hello_git_svn/hello_git_svn/hello.cpp
+++ hello_git_svn/hello_git_svn/hello.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include "test.h"
 
 int main()
 {
 
 	std::cout << "hello git and svn!!" << std::endl;
 	std::cout << " 1st update" << std::endl;
+	std::cout << " 2nd update!!" << std::endl;
 
 
 	return 0;
